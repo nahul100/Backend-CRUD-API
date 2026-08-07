2c26138114507a304f7efd75abfeeb2b2175a5cf
Nahul <205106nahuljulkernain@gmail.com>
2026-08-07 21:47:41 +0600

    second commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const app = express();
 const PORT = 3000;
-
+const tasks = require('./MOCK_DATA.json');
 //const openapi = YAML.load('./openapi.yaml');
 const swaggerDocument = YAML.load('./swagger.yaml');
 
@@ -12,6 +12,23 @@ app.use('/docs', swaggerUi.serve,swaggerUi.setup(swaggerDocument));
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+app.get('/', (req, res) => {
+  res.json({
+    name: "Task API",
+    version: "1.0",
+    endpoints: ["/tasks"]
+  });
+});
+
+// Health Check
+app.get('/health', (req, res) => {
+  res.json({
+    status: "ok"
+  });
+});
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
